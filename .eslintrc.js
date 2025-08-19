72c648c9ec372a6b28f5b5ef83db94b306f539ea
CesarLoor <ciloor2@espe.edu.ec>
2025-08-19 09:14:28 -0500

    error que tenía la pagina

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+    'indent': ['error', 2],
+    'linebreak-style': ['error', 'windows'],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always'],
+    'no-console': 'warn',
+    'no-unused-vars': 'warn'
+  }
+};
